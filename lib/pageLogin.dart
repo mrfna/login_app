c98b85ec4ccc57738ed8492446520bfd62f0f03b
Erwan Widayat <erwan.wid@gmail.com>
2021-11-07 19:47:43 +0700

    Lihat Penggunaan Koma Biar Lebih rapih

--- lib/pageLogin.dart
+++ lib/pageLogin.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class PageLogin extends StatefulWidget {
@@ -10,62 +9,101 @@ class PageLogin extends StatefulWidget {
 }
 
 class _PageLoginState extends State<PageLogin> {
+  // Ketika Button Login Di Tekan Akan Menjalankan Action Ini
+  void prosesLogin() {
+    print('ok');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login Page'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Login', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            SizedBox(
-              height: 16,
-            ),
-            Text('Silahkan login terlebih dahulu untuk menggunakan aplikasi', style: TextStyle(fontSize: 16, color: Colors.grey[600]),),
-            SizedBox(
-              height: 16,
-            ),
-            Text('Username', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-            SizedBox(
-              height: 16,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),
-                labelText: 'Masukkan Username'
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Login',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text('Password', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-            SizedBox(
-              height: 16,
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),
-                labelText: 'Masukkan Password', 
+              SizedBox(
+                height: 16,
               ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: () {}, 
-                child: Text('Login', style: TextStyle(fontSize: 16),) 
+              Text(
+                'Silahkan login terlebih dahulu untuk menggunakan aplikasi',
+                style: TextStyle(fontSize: 16, color: Colors.grey[600]),
               ),
-            )
-          ],
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Username',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Masukkan Username',
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Masukkan Password',
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  onPressed: () {
+                    prosesLogin();
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-        ),
+      ),
     );
   }
 }
